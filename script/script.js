e28dd4ee8e6d082f4ecfd6e711407e12d49fd308
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-13 15:26:41 -0500

    Temas funcionando

--- script/script.js
+++ script/script.js
@@ -73,3 +73,31 @@ mostrarOperacion(multiplicar);
 mostrarOperacion(punto);
 
 calcularOperacion(igual);
+
+// ------------temas----------------------------------------------------
+
+const tema2 = document.getElementById('tema_2');
+tema2.addEventListener('click', () => {
+  console.log('tema2');
+  const body = document.querySelector('body');
+  body.classList.add('active2');
+  body.classList.remove('active1');
+  body.classList.remove('active3');
+});
+
+const tema1 = document.getElementById('tema_1');
+tema1.addEventListener('click', () => {
+  console.log('tema1');
+  const body = document.querySelector('body');
+  body.classList.add('active1');
+  body.classList.remove('active2');
+  body.classList.remove('active3');
+});
+
+const tema3 = document.getElementById('tema_3');
+tema3.addEventListener('click', () => {
+  console.log('tema3');
+  const body = document.querySelector('body');
+  body.classList.add('active3');
+  body.classList.remove('active2');
+});
